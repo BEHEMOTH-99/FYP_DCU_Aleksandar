a8a7d90989b9177b1e48c62f8b88763de4c22da2
BEHEMOTH-99 <aleksstanivukovic@gmail.com>
2026-03-17 22:42:13 +0000

    Fix OnDataSent callback argument type
    Updated OnDataSent callback to use correct argument type for Core 3.0.

--- Arduino_Code/Sender/Sender.ino
+++ Arduino_Code/Sender/Sender.ino
@@ -17,8 +17,8 @@ typedef struct struct_message {
 struct_message myData;
 esp_now_peer_info_t peerInfo;
 
-// Callback when data is sent
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+/ FIX: The first argument MUST be 'const wifi_tx_info_t *info' for Core 3.0+
+void OnDataSent(const wifi_tx_info_t *info, esp_now_send_status_t status) {
   // Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
